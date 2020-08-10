31c84c9decdcd3c2a43616fd3bc5cbf5980c0464
Conrad <conrad2007@gmail.com>
2020-08-10 14:02:38 +0200

    commit

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 8, ErrorMessage = "Password required!")]
+        public string Password { get; set; }
+    }
+}
